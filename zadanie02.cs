6d037ac037bd2f61bb8b30c7ad0ddba7b746eeba
Wanessa Witkowska <128226892+Shugisia@users.noreply.github.com>
2023-06-20 19:14:41 +0200

    Add files via upload

---
+++ zadanie02.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main()
+    {
+        int rozmiar, i;
+
+        rozmiar = 10;
+        int[] tab = new int[rozmiar];
+
+        for (i = 0; i <= rozmiar - 1; i++)
+        {
+            tab[i] = random.Next(100);
+        }
+        for (i = 0; i <= rozmiar - 1; i++)
+        {
+            Console.WriteLine(tab[i]);
+        }
+
+        int najmniejszaWartosc = ZnajdzNajmniejszy(tab);
+        int najwiekszaWartosc = ZnajdzNajwiekszy(tab);
+
+        Console.WriteLine("Najmniejszy element: " + najmniejszaWartosc);
+        Console.WriteLine("Największy element: " + najwiekszaWartosc);
+    }
+
+    public static int ZnajdzNajmniejszy(int[] tab)
+    {
+        int najmniejszaWartosc = tab[0];
+
+        for (int i = 1; i < tab.Length; i++)
+        {
+            if (tab[i] < najmniejszaWartosc)
+            {
+                najmniejszaWartosc = tab[i];
+            }
+        }
+
+        return najmniejszaWartosc;
+    }
+
+    public static int ZnajdzNajwiekszy(int[] tab)
+    {
+
+        int najwiekszaWartosc = tab[0];
+
+        for (int i = 1; i < tab.Length; i++)
+        {
+            if (tab[i] > najwiekszaWartosc)
+            {
+                najwiekszaWartosc = tab[i];
+            }
+        }
+
+        return najwiekszaWartosc;
+    }
+}
---
+++ zadanie03.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+public class MyProgram
+{
+    private static Random random = new Random();
+
+    public static void Main(string[] args)
+    {
+        int rOZMIAR, i;
+
+        rOZMIAR = 10;
+        int[] tab = new int[rOZMIAR];
+
+        for (i = 0; i <= rOZMIAR - 1; i++)
+        {
+            tab[i] = random.Next(100);
+        }
+      
+        Console.WriteLine("Przed sortowaniem:");
+        Wypisz(tab);
+
+        SortowanieBabelkowe(tab);
+
+        Console.WriteLine("Po sortowaniu:");
+        Wypisz(tab);
+    }
+
+    public static void SortowanieBabelkowe(int[] tab)
+    {
+        int n = tab.Length;
+        bool zmiana;
+
+        for (int i = 0; i < n - 1; i++)
+        {
+            zmiana = false;
+
+            for (int j = 0; j < n - i - 1; j++)
+            {
+                if (tab[j] > tab[j + 1])
+                {
+                    // Zamiana miejscami
+                    int temp = tab[j];
+                    tab[j] = tab[j + 1];
+                    tab[j + 1] = temp;
+
+                    zmiana = true;
+                }
+            }
+
+            // Jeżeli w danym przejściu nie było żadnej zamiany, to lista jest już posortowana
+            if (!zmiana)
+            {
+                break;
+            }
+        }
+    }
+
+    public static void Wypisz(int[] tab)
+    {
+        foreach (int element in tab)
+        {
+            Console.Write(element + " ");
+        }
+        Console.WriteLine();
+    }
+}
+    
+
